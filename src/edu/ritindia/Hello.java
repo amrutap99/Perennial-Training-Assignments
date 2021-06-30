e212b4a5dfa8debc16c4d45c36efd38a5095581f
Amruta <adiraamruta99@gmail.com>
2021-06-30 12:11:48 +0530

    Add File

---
+++ src/edu/ritindia/Hello.java
@@ -0,0 +1,46 @@
+package edu.ritindia;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello
+ */
+@WebServlet("/Hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Hello() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	
+	
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();  //Java PrintWriter class is the implementation of Writer class. 
+												//It is used to print the formatted representation of objects to the text-output stream.
+		
+		String name=request.getParameter("user");
+		out.print("Hello "+name);
+		
+		
+		out.close();
+	}
+
+}
