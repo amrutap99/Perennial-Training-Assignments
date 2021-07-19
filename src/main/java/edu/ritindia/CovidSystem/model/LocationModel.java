51392ab753a96d820c790310bce5aa03bbcd564c
Amruta <adiraamruta99@gmail.com>
2021-07-19 09:59:06 +0530

    Mention Specific Import

--- src/main/java/edu/ritindia/CovidSystem/model/LocationModel.java
+++ src/main/java/edu/ritindia/CovidSystem/model/LocationModel.java
@@ -83,7 +83,6 @@ public class LocationModel {
         return diffFromPrevDay;
     }
 
-
     public void setDiffFromPrevDay(int diffFromPrevDay) {
         this.diffFromPrevDay = diffFromPrevDay;
     }
